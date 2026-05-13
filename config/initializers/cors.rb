c585a4c30afd1fffbd129b4bfa2ca64fdd208a71
ana <ana.souza0087@gmail.com>
2026-05-13 17:58:33 -0300

    fix: update allowed frontend origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:5173"
+    origins "http://localhost:5173", "https://project-real-time-chat.vercel.app"
 
     resource "*",
       headers: :any,
